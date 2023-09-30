470a4e233b165c21e8926332c2bb70dd80d79675
oluizleme <o.luizleme@gmail.com>
2023-09-30 15:50:51 -0300

    (feat): Implementadas as classes de infraestrutura

---
+++ escola/src/main/java/br/com/oluizleme/escola/dominio/aluno/RepositorioDeAlunos.java
@@ -0,0 +1,10 @@
+package br.com.oluizleme.escola.dominio.aluno;
+
+import java.util.List;
+
+public interface RepositorioDeAlunos {
+
+	void matricular(Aluno aluno);
+	Aluno buscarPorCPF(CPF cpf);
+	List<Aluno> listarTodosAlunosMatriculados();
+}
